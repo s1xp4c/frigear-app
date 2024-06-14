ae540add6af331e979f9d0e58d841d586d9b8401
hareland <packagist@proton.me>
2024-06-14 17:39:58 +0200

    remove redundant comment

--- lib/repositories/product/product-repository.ts
+++ lib/repositories/product/product-repository.ts
@@ -1,21 +1,3 @@
-//  id               uuid    not null         default gen_random_uuid() primary key,
-//     slug             text    not null unique,
-//     stripe_id        text                     default null,
-//     last_stripe_sync timestamp with time zone default null,
-//     name             text    not null,
-//     active           boolean not null         default true,
-//     description      text                     default null,
-//     features         jsonb   not null         default '{}'::jsonb,
-//     tags             jsonb   not null         default '[]'::jsonb,
-//     related_ids      jsonb   not null         default '[]'::jsonb,
-//     replaced_by      uuid                     default null references public.product (id) on delete cascade,
-//     meta             jsonb   not null         default '{}'::jsonb,
-//     metrics          jsonb   not null         default '{}'::jsonb,
-//     discontinued_at  timestamp with time zone default null,
-//     created_at       timestamp with time zone default now(),
-//     updated_at       timestamp with time zone default null
-import {KeepOptional, StripOptional} from "@/lib/types";
-
 type JsonBField = Record<string, any>;
 export type RequiredProductFields = 'slug' | 'name';
 
