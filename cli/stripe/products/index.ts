5afc08a9b8819c8d4e0715e39e9893ccff8fd159
hareland <packagist@proton.me>
2024-06-14 17:39:21 +0200

    remove redundant quotes

--- cli/stripe/products/index.ts
+++ cli/stripe/products/index.ts
@@ -4,13 +4,13 @@ import {apiContainer} from "@/app/api/api-container";
 
 export default defineCommand({
     subCommands: {
-        'import': defineCommand({
+        import: defineCommand({
             async run() {
                 const service = apiContainer.get('stripeProductIOService');
                 await service.importProductsFromStripe();
             }
         }),
-        'list': defineCommand({
+        list: defineCommand({
             async run({}) {
                 const service = apiContainer.get('stripeProductService');
                 console.table(service.all());
